fac660c4168d83b690e1531719644177e61dba33
SaaberDev <saaberdev@gmail.com>
2022-05-09 04:38:33 +0600

    [improvements] - design and logo change

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -256,7 +256,7 @@
 @endsection
 
 @push('scripts')
-    <script src="{{ mix('_assets/plugins/chart.js/chart.js') }}"></script>
+    <script src="{{ mix('_assets/plugins/chart-js/chart.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
